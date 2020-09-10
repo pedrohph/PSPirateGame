8635db5141660094acdb06b3a5816315e979a4b7
pedrohph <micosacpv@hotmail.com>
2020-09-10 02:55:35 -0300

    Player movement and attacks

---
+++ Assets/Scripts/CannonBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    public float bulletSpeed;
+    public int bulletDamage;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * bulletSpeed * Time.deltaTime);   
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Destroy(gameObject);
+    }
+
+    public void Setup(int damage, Collider2D creatorCollider) {
+        bulletDamage = damage;
+        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), creatorCollider);
+    }
+}
---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour {
+    public GameObject bullet;
+    public float playerSpeed;
+    public int playerDamage;
+
+    private Transform FrontCannon;
+    private Transform[] LateralCannon = new Transform[8];
+
+    [SerializeField] private Collider2D shipCollider;
+    // Start is called before the first frame update
+    void Start() {
+        FrontCannon = gameObject.transform.GetChild(0);
+        for(int i = 0; i<6; i++) {
+            LateralCannon[i] = gameObject.transform.GetChild(i + 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Remover ao fazer o sistema de controles
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            MoveForward();
+        }
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            FrontShoot();
+        }else if (Input.GetKeyDown(KeyCode.X)) {
+            SideShoot();
+        }
+        RotateShip(Input.GetAxis("Horizontal"));
+    }
+
+    public void MoveForward() {
+        transform.Translate(Vector3.down * Time.deltaTime * playerSpeed);
+    }
+
+    public void RotateShip(float direction) {
+        transform.Rotate(0, 0, direction * -1);
+    }
+
+
+    public void FrontShoot() {
+        Instantiate(bullet,FrontCannon.position, FrontCannon.rotation).GetComponent<CannonBall>().Setup(playerDamage, shipCollider);
+    }
+
+    public void SideShoot() {
+        for(int i = 0; i<6; i++) {
+            Instantiate(bullet, LateralCannon[i].position, LateralCannon[i].rotation).GetComponent<CannonBall>().Setup(playerDamage, shipCollider);
+        }
+    }
+}
